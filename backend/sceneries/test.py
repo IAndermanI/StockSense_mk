e5938546fa9f38ecee8babac888e73f445929120
IAndermanI <anpavlov2004@gmail.com>
2024-11-13 13:00:08 +0300

    Added a scenery. Changed structure of scenery creation

---
+++ backend/sceneries/test.py
@@ -0,0 +1,13 @@
+test_1 = {
+    "test": [1, 1],
+    "item": [2, 2],
+    "thing": [600, 600]
+}
+
+test_2 = {
+    "test": [10, 10],
+    "item": [20, 20],
+    "thing": [600, 600]
+}
+
+test_scenery = [test_1, test_2]
